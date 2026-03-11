dca38e55fc0c27dbbe32b6497a0fed14cca3d562
Gavino Carranco <gcarranco@hotmail.com>
2026-03-11 16:15:55 -0500

    Fecha y hora de Ecuador

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,13 @@
 <?php
+// Zona horaria por defecto de la aplicación (UTC-05, Ecuador continental)
+date_default_timezone_set('America/Guayaquil');
+
 $conn= new mysqli('localhost','root','','db_quiz')or die("Could not connect to mysql".mysqli_error($con));
 // Agregar esta línea
 $conn->set_charset("utf8mb4");
+
+// Alinear la sesión de MySQL con UTC-05
+$conn->query("SET time_zone = '-05:00'");
 
 // Asegurar que la columna level_id existe en quiz_list
 $col_check = $conn->query("SHOW COLUMNS FROM quiz_list LIKE 'level_id'");
